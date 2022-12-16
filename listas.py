876893e33fb9c5a35e236604f41a7a6f00188c48
Edmoun Ramirez <edmounramirez@gmail.com>
2022-12-15 21:29:44 -0400

    listas

---
+++ listas.py
@@ -0,0 +1,17 @@
+# Apartado 1: Repaso de conceptos basicos.
+numero = [1, 2, 3, 4, 5]
+print(numero)
+
+primerValor = numero[0]
+print(primerValor)
+
+longitud = len(numero)
+print(f"El primer valor es: {primerValor}\nLa logitud de la lista es: {longitud}")
+
+for num in numero:
+    print(num)
+# Apartado 2: Indexado y sublistas.
+
+# Apartado 3: Comprobar si una lista contiene o no un elemento.
+
+# Modificar elementos en una lista.
