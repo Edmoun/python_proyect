21decbef53ef17dcc7a589c9a7077b1f3c080f28
Edmoun Ramirez <edmounramirez@gmail.com>
2022-12-21 21:15:20 -0400

    ejercicio5

---
+++ ejercicio5.py
@@ -0,0 +1,21 @@
+"""Ejercicio 1.10.5. Escribir un programa que pregunte al usuario:
+
+su nombre, y luego lo salude.
+dos números y luego muestre el producto."""
+
+def saludar():
+    name = input("Escribe tu nombre: ")
+    name = str(name)
+
+    if type(name) == str:
+        print(f"hola {name}")
+    else:
+        print("Escribe un nombre")
+
+def producto():
+    num1 = int(input("Escribre tu primer numero: "))
+    num2 = int(input("Escribre tu segundo numero: "))
+
+    print(num1*num2)
+
+saludar(),producto()
