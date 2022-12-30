5166af04d6315075533c6fc8fae7e08bb13a82a5
Edmoun Ramirez <edmounramirez@gmail.com>
2022-12-30 12:06:38 -0400

    ejercicio7

---
+++ ejercicio7.py
@@ -0,0 +1,8 @@
+# Ejercicio 7: Escribir un ciclo definido que salude por pantalla a sus cinco mejores amigos/as.
+
+for amigos in range(5):
+    amigos = input("Escribe tu nombre amigo mio: ")
+    print("Hola " + amigos + " te quiero")
+
+   
+
